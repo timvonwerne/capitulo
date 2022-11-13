8f74657911d1e3c4224c8846cffe5c3933a59e79
Tim von Werne <me@vonwernet.de>
2022-11-13 18:14:50 +0100

    Cover command

---
+++ commands/cover.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import chalk from 'chalk';
+import fs from 'fs';
+
+const cover = async (asin, { region, debug }) => {
+  var c = '';
+  if (asin) {
+    await axios
+      .get(`https://api.audnex.us/books/${asin}`, {
+        params: {
+          region,
+        },
+      })
+      .then((response) => {
+        c = response.data.image;
+
+        // If the cover URL is not empty, download the cover
+        if (c !== '') {
+          if (!debug) {
+            axios({
+              method: 'get',
+              url: c,
+              responseType: 'stream',
+            }).then((response) => {
+              response.data.pipe(fs.createWriteStream(`cover.jpg`));
+            });
+          } else {
+            console.log(chalk.green('Cover URL: ') + c);
+          }
+        } else {
+          console.error(chalk.red('No cover found'));
+          process.exit(1);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        console.error(chalk.red(error.response.data.message));
+        process.exit(1);
+      });
+  } else {
+    console.error(chalk.red('No ASIN provided'));
+    process.exit(1);
+  }
+};
+
+export default cover;
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { program } from 'commander';
 import chalk from 'chalk';
 import chapters from './commands/chapters.js';
 import description from './commands/description.js';
+import cover from './commands/cover.js';
 
 program
   .command('chapters <asin>')
@@ -22,5 +23,15 @@ program
     false
   )
   .action(description);
+
+program
+  .command('cover <asin>')
+  .option('-r, --region <region>', 'Region to use for the request')
+  .option(
+    '-d, --debug',
+    'Setting this flag will prevent the program from creating a file. The output will be printed to the console instead.',
+    false
+  )
+  .action(cover);
 
 program.parse();
