9f88e6544f9dfdcb561eab2b98150d9326de33cd
Tim von Werne <me@vonwernet.de>
2022-11-13 18:10:37 +0100

    Enable file creation for chapters

--- commands/chapters.js
+++ commands/chapters.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import chalk from 'chalk';
 import formatTime from '../util/time-formatter.js';
+import fs from 'fs';
 
-const chapters = async (asin, { region }) => {
+const chapters = async (asin, { region, debug }) => {
   var l = 0;
   var c = [];
+  var cf = '';
 
   if (asin) {
     await axios
@@ -21,6 +23,16 @@ const chapters = async (asin, { region }) => {
               timestamp: formatTime(l),
             });
 
+            cf += `${formatTime(l)} ${chapter.title}`;
+
+            // If the chapter is not the last chapter, add a new line
+            if (
+              response.data.chapters.indexOf(chapter) !==
+              response.data.chapters.length - 1
+            ) {
+              cf += '\n';
+            }
+
             l += chapter.lengthMs;
           });
         }
@@ -30,18 +42,26 @@ const chapters = async (asin, { region }) => {
         process.exit(1);
       });
 
-    return c;
+    if (!debug) {
+      fs.writeFile(
+        `chapters.txt`,
+        cf,
+        { flag: 'wx' },
+        (err) => err && console.error(chalk.red(err))
+      );
+    } else {
+      printChapters(c);
+    }
   } else {
     console.error(chalk.red('No ASIN provided'));
     process.exit(1);
   }
 };
 
-const printChapters = async (asin, options) => {
-  const c = await chapters(asin, options);
-  if (c !== '') {
+const printChapters = async (chapters) => {
+  if (chapters.length > 0) {
     console.log('The following chapters were found:');
-    c.forEach((chapter) => {
+    chapters.forEach((chapter) => {
       console.log(
         `${chalk.bgGray.white(chapter.timestamp)} ${chalk.blue(chapter.title)}`
       );
@@ -49,4 +69,4 @@ const printChapters = async (asin, options) => {
   }
 };
 
-export { chapters, printChapters };
+export default chapters;
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 #! /usr/bin/env node
 import { program } from 'commander';
 import chalk from 'chalk';
-import { chapters, printChapters } from './commands/chapters.js';
+import chapters from './commands/chapters.js';
 import description from './commands/description.js';
 
 program
   .command('chapters <asin>')
   .option('-r, --region <region>', 'Region to use for the request')
-  .action(printChapters);
+  .option(
+    '-d, --debug',
+    'Setting this flag will prevent the program from writing the chapter list to a file. The output will be printed to the console instead.'
+  )
+  .action(chapters);
 
 program
   .command('description <asin>')
